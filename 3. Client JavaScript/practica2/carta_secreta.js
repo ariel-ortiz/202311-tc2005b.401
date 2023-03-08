bb16a5134d7cd0842a73a9311977ada9a948e4b8
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2023-03-08 00:53:57 +0000

    Agrega código para la funcionalidad completa de la web app

--- 3. Client JavaScript/practica2/carta_secreta.js
+++ 3. Client JavaScript/practica2/carta_secreta.js
@@ -1,7 +1,6 @@
-const cartas = inicia_cartas();
-console.log(cartas);
+const cartas = iniciaCartas();
 
-function inicia_cartas() {
+function iniciaCartas() {
   const cartas = [];
   for (let palo of 'CDHS') {
     for (let i = 1; i <= 13; i++) {
@@ -9,5 +8,28 @@ function inicia_cartas() {
       cartas.push(nombre);
     }
   }
+  shuffle(cartas);
   return cartas;
+}
+
+// Tomado de:
+// https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+function shuffle(a) {
+  for (let i = a.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let t = a[i];
+    a[i] = a[j];
+    a[j] = t;
+  }
+}
+
+function descubreCarta() {
+  if (cartas.length > 0) {
+    let carta = cartas.pop();
+    contador.innerText = cartas.length.toString();
+    laCarta.src = 'cards/' + carta;
+  } else {
+    miBoton.style.display = 'none';
+    reiniciar.style.display = 'block';
+  }
 }
