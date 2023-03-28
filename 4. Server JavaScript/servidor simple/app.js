ff73940fff0c269edf524ae2627ba3e53929d34e
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2023-03-28 21:27:36 +0000

    Agrega código para implementar API web de GCD

--- 4. Server JavaScript/servidor simple/app.js
+++ 4. Server JavaScript/servidor simple/app.js
@@ -10,6 +10,31 @@ app.get('/', (req, res) => {
   res.send('hola mundo');
 });
 
+app.get('/gcd/:num1/:num2', (req, res) => {
+  let x = parseInt(req.params.num1);
+  let y = parseInt(req.params.num2);
+  if (isNaN(x) || isNaN(y)) {
+    res.json({
+      status: 'error',
+      message: 'one of the input values is not a number'
+    });
+  } else {
+    res.json({status: 'ok', result: gcd(x, y)});
+  }
+});
+
+function gcd(a, b) {
+  while (b != 0) {
+    [a, b] = [b, a % b];
+  }
+  return a;
+}
+
+// Página 404
+app.use((req, res) => {
+  res.type('text/plain').status(404).send('404 - Not Found');
+});
+
 app.listen(port, () => {
   console.log(`Express inició en http://${ipAddr}:${port}`
     + '\nPresiona Ctrl-C para terminar.');
